aa932fb4edd247d08ef17033b9d93d1bc8b4b1f7
kimsohyeon <aotoyae@gmail.com>
2023-09-26 22:09:59 +0900

    first commit

---
+++ js/baekjoon/bkjn_1001_1.js
@@ -0,0 +1,10 @@
+// const input = require("fs").readFileSync(0, 'utf8').split(" ");
+// console.log(Number(input[0]) - Number(input[1]));
+
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").split(" ");
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+
+console.log(a - b);
