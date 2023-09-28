b740ae836ad42d1fb4ff5fa2cd9e53fb281512ef
kimsohyeon <aotoyae@gmail.com>
2023-09-28 23:43:07 +0900

    second commit

--- js/baekjoon/step2/bkjn_2525_1.js
+++ js/baekjoon/step2/bkjn_2525_1.js
@@ -4,9 +4,6 @@ let hour = parseInt(input[0].split(" ")[0]);
 let minute = parseInt(input[0].split(" ")[1]);
 let cook = parseInt(input[1]);
 
-let h = 0;
-let m = 0;
-
 h = Math.floor((hour * 60 + minute + cook) / 60);
 m = (hour * 60 + minute + cook) % 60;
 if (h >= 24) {
