09d932e76abb47b367fd636db8ed07928cd433d9
kimsohyeon <aotoyae@gmail.com>
2023-10-04 15:22:53 +0900

    second commit

--- js/baekjoon/step4/bkjn_10810_1.js
+++ js/baekjoon/step4/bkjn_10810_1.js
@@ -4,13 +4,17 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-let [n, m] = input[0].split(" ").map(Number);
-let baskets = new Array(n).fill(0);
+const [n, m] = input[0].split(" ").map(Number);
+let baskets = Array(n);
 
-for (i = 0; i <= m; i++) {
-  const [start, end, ball] = input[i].split(" ").map(Number);
+for (i = 1; i <= m; i++) {
+  let [start, end, ball] = input[i].split(" ").map(Number);
   for (j = start; j <= end; j++) {
     baskets[j - 1] = ball;
   }
 }
+for (k = 0; k < baskets.length; k++) {
+  typeof baskets[k] === "undefined" ? (baskets[k] = 0) : baskets[k];
+}
+
 console.log(baskets.join(" "));
