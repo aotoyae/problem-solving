868e060c198befbb687eab397550fe251d06f751
kimsohyeon <aotoyae@gmail.com>
2023-10-09 21:11:21 +0900

    second commit

--- js/programmers/beginner/prgm_120811_1.js
+++ js/programmers/beginner/prgm_120811_1.js
@@ -1,4 +1,6 @@
 function solution(array) {
   let answer = array.sort((a, b) => a - b);
   return answer[Math.floor(answer.length / 2)];
+  // return array.sort((a, b) => a - b)[Math.floor(answer.length / 2)]
+  // >> 한 줄로 가능
 }
--- js/programmers/beginner/prgm_181939_1 copy.js
+++
@@ -1,6 +0,0 @@
-function solution(a, b) {
-  const ab = String(a) + String(b);
-  const ba = String(b) + String(a);
-
-  return ab > ba ? Number(ab) : Number(ba);
-}
