40660e3147f43eec6c098d0d89d2f27a892c9357
kimsohyeon <aotoyae@gmail.com>
2023-10-30 20:44:48 +0900

    second commit

--- js/programmers/level.0/prgm_120882_1.js
+++ js/programmers/level.0/prgm_120882_1.js
@@ -1,3 +1,5 @@
 function solution(score) {
-  for (i = 0; i < score.length; i++) {}
+  let avg = score.map((v) => (v[0] + v[1]) / 2);
+  let ranking = [...avg].sort((a, b) => b - a);
+  return avg.map((v) => ranking.indexOf(v) + 1);
 }
