5e930d02907280ed3e7f757b5788a4f5eccb09ce
kimsohyeon <aotoyae@gmail.com>
2024-01-03 11:13:46 +0900

    first commit

---
+++ js/programmers/level.1/prgm_12915_1.js
@@ -0,0 +1,17 @@
+function solution(strings, n) {
+  let result = [];
+
+  for (let i = 0; i < strings.length; i++) {
+    result[i] = strings[i][n] + strings[i];
+  }
+
+  result.sort();
+
+  for (let j = 0; j < result.length; j++) {
+    result[j] = result[j].slice(1);
+  }
+
+  console.log(result);
+}
+
+solution(["sun", "bed", "car"], 1);
