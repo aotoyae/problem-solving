e386b7c4a0f1242299392e94ad6772c6340cc0c9
kimsohyeon <aotoyae@gmail.com>
2024-02-08 10:08:42 +0900

    add: level.1

---
+++ js/programmers/level.1/prgm_135808_1.js
@@ -0,0 +1,11 @@
+function solution(k, m, score) {
+  let answer = 0;
+  let box = Math.floor(score.length) / m;
+  score.sort((a, b) => b - a);
+
+  for (let i = 1; i <= box; i++) {
+    answer += score[m * i - 1] * m;
+  }
+
+  return answer;
+}
