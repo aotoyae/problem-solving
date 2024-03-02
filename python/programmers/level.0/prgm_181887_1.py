7ce9d8083cfc6490a117d4cfc3f5e981f2206bd1
kimsohyeon <aotoyae@gmail.com>
2024-03-02 21:44:27 +0900

    Add: level.0

---
+++ python/programmers/level.0/prgm_181887_1.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    a = 0
+    b = 0
+
+    for i in range(len(num_list)):
+        if i%2 == 1:
+            a += num_list[i]
+        else:
+            b += num_list[i]
+
+    return max(a, b)
