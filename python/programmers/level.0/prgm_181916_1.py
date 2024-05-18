c1ee9a41e41586a56fcc4d930da3075ad18883dc
kimsohyeon <aotoyae@gmail.com>
2024-05-18 12:44:16 +0900

    add: level.0

---
+++ python/programmers/level.0/prgm_181916_1.py
@@ -0,0 +1,20 @@
+def solution(a, b, c, d):
+    if a == b and a == c and a == d: return 1111 * a
+
+    if a == b and a == c: return (10 * a + d) ** 2
+    if a == b and a == d: return (10 * a + c) ** 2
+    if a == c and a == d: return (10 * a + b) ** 2
+    if b == c and b == d: return (10 * b + a) ** 2
+
+    if a == b and c == d: return (a + c) * abs(a - c)
+    if a == c and b == d: return (a + b) * abs(a - b)
+    if a == d and b == c: return (a + b) * abs(a - b)
+
+    if a == b: return c * d
+    if a == c: return b * d
+    if a == d: return b * c
+    if b == c: return a * d
+    if b == d: return a * c
+    if c == d: return a * b
+
+    return min(a, b, c, d)
