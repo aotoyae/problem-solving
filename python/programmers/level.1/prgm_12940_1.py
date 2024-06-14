eb7114a6c5750d83f4cb39b72de3609adff290ad
kimsohyeon <aotoyae@gmail.com>
2024-06-14 14:35:14 +0900

    add: level.1

---
+++ python/programmers/level.1/prgm_12940_1.py
@@ -0,0 +1,15 @@
+def solution(n, m):
+    divisor = 0
+    multiple = 0
+
+    for i in range(min(n, m), 0, -1):
+        if n%i == 0 and m%i == 0:
+            divisor = i
+            break
+    
+    for i in range(max(n, m), (n*m)+1):
+        if i%n == 0 and i%m == 0:
+            multiple = i
+            break
+    
+    return [divisor, multiple]
