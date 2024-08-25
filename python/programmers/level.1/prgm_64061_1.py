698814ad45eac56c89172d02675d600c63ff2be3
kimsohyeon <aotoyae@gmail.com>
2024-08-25 23:58:32 +0900

    fix: level.1

--- python/programmers/level.1/prgm_64061_1.py
+++ python/programmers/level.1/prgm_64061_1.py
@@ -3,14 +3,15 @@ def solution(board, moves):
     count = 0
 
     for i in moves:
-        for  j in board[i - 1]:
-            for k in j:
-                if k == 0: continue
-                else:
-                    if bucket[-1] == k:
-                        bucket.pop()
-                        count += 2 
-                    else: bucket.append(k)
+        for j in board:
+            if j[i - 1] != 0:
+                if bucket and bucket[-1] == j[i - 1]:
+                    bucket.pop()
+                    count += 2 
+                else: bucket.append(j[i - 1])
+                j[i - 1] = 0
+                break
+                
 
     return count
                         
