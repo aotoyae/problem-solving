b9dff2057bc1b0c58b0e50a3c6690f91502ab457
kimsohyeon <aotoyae@gmail.com>
2024-09-01 21:44:54 +0900

    add: step3

---
+++ python/baekjoon/step3/bkjn_25206_1.py
@@ -0,0 +1,15 @@
+rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+
+total = 0
+score = 0
+
+for _ in range(20):
+    a, b, c = input().split()
+    b = float(b)
+
+    if c != 'P':
+        total += b
+        score += b * grade[rating.index(c)]
+
+print(format(score / total, '.6f'))
