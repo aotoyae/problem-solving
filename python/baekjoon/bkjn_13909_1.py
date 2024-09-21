7ac7e4f833e9dc0f67acbde31a5312eb381962e3
kimsohyeon <aotoyae@gmail.com>
2024-09-21 15:26:20 +0900

    fix: bkjn 13909

--- python/baekjoon/bkjn_13909_1.py
+++ python/baekjoon/bkjn_13909_1.py
@@ -1,11 +1,7 @@
 import sys
-import math
 sys.stdin = open('input.txt', 'r')
 # input = sys.stdin.readline
 
 n = int(input())
-lst = [0] * n
 
-count_of_squares = int(math.sqrt(n))
-
-print(count_of_squares)
+print(int(n ** 0.5))
