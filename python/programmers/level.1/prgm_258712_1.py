2e391dc3126966cf29b9b56f88365636cdbac630
kimsohyeon <aotoyae@gmail.com>
2024-09-24 01:39:44 +0900

    add: prgm 258712

---
+++ python/programmers/level.1/prgm_258712_1.py
@@ -0,0 +1,24 @@
+def solution(friends, gifts):
+    answer = 0
+    gift_dic={k: {j: 0 for j in friends if j != k} for k in friends}
+    gives = {k: 0 for k in friends}
+    gets = {k: 0 for k in friends}
+
+    for gift in gifts:
+        send, target = gift.split()
+        gift_dic[send][target] += 1
+        gives[send] += 1
+        gets[target] += 1
+
+    for k, _ in gift_dic.items():
+        count = 0
+
+        for kk, _ in gift_dic[k].items():
+            if gift_dic[k][kk] > gift_dic[kk][k]:
+                count += 1
+            elif gift_dic[k][kk] == gift_dic[kk][k]:
+                if gives[k] - gets[k] > gives[kk] - gets[kk]:
+                    count += 1
+        answer = max(answer, count)
+
+    return answer
