3523b26489396a851699466a9f220bf3974c89d4
kimsohyeon <aotoyae@gmail.com>
2024-09-28 14:34:03 +0900

    add: bkjn 2839

---
+++ python/baekjoon/bkjn_2839_1.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+# input = sys.stdin.readline
+
+N = int(input())
+cnt = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        cnt += N // 5
+        print(cnt)
+        break
+    N -= 3
+    cnt += 1
+
+else:
+    print(-1)
