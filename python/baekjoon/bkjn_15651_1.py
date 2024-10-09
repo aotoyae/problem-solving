2407e132b602851926f77cba62b36de5ae6d1012
kimsohyeon <aotoyae@gmail.com>
2024-10-09 12:17:45 +0900

    add: bkjn 15651

---
+++ python/baekjoon/bkjn_15651_1.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+# input = sys.stdin.readline
+
+N, M = map(int, input().split())
+lst = []
+
+def dfs():
+    if len(lst) == M:
+        print(' '.join(map(str, lst)))
+        return
+
+    for i in range(1, N + 1):
+        lst.append(i)
+        dfs()
+        lst.pop()
+
+dfs()
