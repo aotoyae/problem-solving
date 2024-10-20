0f0ec3059dcc28f38d7f4496e9f5c5f059c91336
kimsohyeon <aotoyae@gmail.com>
2024-10-20 22:31:27 +0900

    add: bkjn 9372

---
+++ python/baekjoon/bkjn_9372_1.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+# input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+
+    for _ in range(M):
+        a, b = map(int, input().split())
+
+    print(N - 1)
