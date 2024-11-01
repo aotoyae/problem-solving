aba2cba0627fd212d5c1969c2204ade655a2bb7a
kimsohyeon <aotoyae@gmail.com>
2024-11-01 22:40:34 +0900

    add: bkjn 11729

---
+++ python/baekjoon/bkjn_11279_1.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+sys.stdin = open('input.txt', 'r')
+# input = sys.stdin.readline
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    x = int(input())
+
+    if x == 0:
+        print(-heapq.heappop(heap)) if heap else print(0)
+    else:
+        heapq.heappush(heap, -x)
