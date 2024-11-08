b2338bf38130995b64272f4f16dc4aa1aa0a6d69
kimsohyeon <aotoyae@gmail.com>
2024-11-08 22:29:49 +0900

    add: bkjn 1138

---
+++ python/baekjoon/bkjn_1138_1.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+# input = sys.stdin.readline
+
+N = int(input())
+lst = list(map(int, input().split()))
+answer = [0] * N
+
+for i in range(N):
+    cnt = 0
+
+    for j in range(N):
+        if cnt == lst[i] and answer[j] == 0:
+            answer[j] = i + 1
+            break
+        elif answer[j] == 0:
+            cnt += 1
+
+print(*answer)
