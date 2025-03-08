0df83a43929901cb6dd18d95767358397100224e
kimsohyeon <aotoyae@gmail.com>
2025-03-08 11:21:46 +0900

    fix: bkjn 6322

--- python/baekjoon/bkjn_6322_1.py
+++ python/baekjoon/bkjn_6322_1.py
@@ -13,13 +13,13 @@ while True:
     print(f"Triangle #{i}")
 
     if c == -1:
-        print("c = %.3f" % ((a ** 2 + b ** 2) ** 0.5))
+        print(f"c = {((a ** 2 + b ** 2) ** 0.5):.3f}")
     elif max(a, b) >= c:
         print("Impossible.")
     elif a == -1:
-        print("a = %.3f" % ((c ** 2 - b ** 2) ** 0.5))
+        print(f"a = {((c ** 2 - b ** 2) ** 0.5):.3f}")
     elif b == -1:
-        print("b = %.3f" % ((c ** 2 - a ** 2) ** 0.5))
+        print(f"b = {((c ** 2 - a ** 2) ** 0.5):.3f}")
 
     i += 1
     print()
