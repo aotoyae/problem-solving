4d740133b6b4f0fff312a03ddbfbb6fe763e4e55
kimsohyeon <aotoyae@gmail.com>
2025-04-16 23:47:14 +0900

    add: bkjn 31995

---
+++ python/baekjoon/bkjn_31995_1.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+# input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+
+if N == 1 or M == 1:
+    print(0)
+else:
+    print((N - 1) * (M - 1) * 2)
